d687f8752b2e09bde86c46cca6de33506e1af5a8
Saidumarov <saidumarovjafarxon@gmail.com>
2024-05-13 08:07:15 +0500

    .

--- src/app/users/api/[id]/route.ts
+++ src/app/users/api/[id]/route.ts
@@ -1,59 +1,8 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import data from "../data.json";
-// export function GET(
-//   request: Request,
-//   context: {
-//     params: {
-//       id: string;
-//     };
-//   }
-// ) {
-//   const id = +context.params.id;
-//   if (id > data.users.length) {
-//     return NextResponse.json(
-//       { message: "Comment not found" },
-//       {
-//         status: 404,
-//       }
-//     );
-//   }
-//   const comment = data.users.find((cm) => cm.id === id);
-//   return Response.json(comment);
-// }
-
-// export function DELETE(
-//   request: Request,
-//   context: {
-//     params: {
-//       id: string;
-//     };
-//   }
-// ) {
-//   const id = +context.params.id;
-//   const index = data.users.findIndex((cm) => cm.id === id);
-//   data.users.splice(index, 1);
-//   return NextResponse.json(data);
-// }
-
-// export async function PUT(
-//   request: Request,
-//   context: {
-//     params: {
-//       id: string;
-//     };
-//   }
-// ) {
-//   const id = +context.params.id;
-//   const comment = await request.json();
-//   const newComments = data.users.map((c) => (c.id === id ? comment : c));
-//   return NextResponse.json(newComments);
-// }
-
 import { NextRequest, NextResponse } from "next/server";
 import { MongoClient, ObjectId } from "mongodb";
 
 const uri =
-  "mongodb+srv://your_username:your_password@cluster0.odgtuwd.mongodb.net/your_database?retryWrites=true&w=majority";
+  "mongodb+srv://saidumarovjafarxon:3hq0F4dsnamY2POo@cluster0.com3cmr.mongodb.net/?retryWrites=true&w=majority";
 
 async function connectToDatabase() {
   const client = new MongoClient(uri);
@@ -75,7 +24,7 @@ export async function GET(
   const id = context.params.id;
   try {
     const usersCollection = await connectToDatabase();
-    const user = await usersCollection.findOne({ _id: ObjectId(id) });
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) });
     if (!user) {
       return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
@@ -92,7 +41,7 @@ export async function DELETE(
   const id = context.params.id;
   try {
     const usersCollection = await connectToDatabase();
-    const result = await usersCollection.deleteOne({ _id: ObjectId(id) });
+    const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
     if (result.deletedCount === 1) {
       return NextResponse.json({ message: "User deleted successfully" });
     } else {
@@ -103,25 +52,24 @@ export async function DELETE(
   }
 }
 
-// export async function PUT(
-//   request: NextRequest,
-//   context: { params: { id: string } }
-// ) {
-//   const id = context.params.id;
-//   const updatedUser = await request.json();
-//   try {
-//     const usersCollection = await connectToDatabase();
-//     const result = await usersCollection.updateOne(
-//       { _id: ObjectId(id) },
-//       { $set: updatedUser }
-//     );
-//     if (result.matchedCount === 1) {
-//       return NextResponse.json({ message: "User updated successfully" });
-//     } else {
-//       return NextResponse.json({ message: "User not found" }, { status: 404 });
-//     }
-//   } catch (error) {
-//     console.error("Error updating user:", error);
-//     return NextResponse.error("Internal Server Error", { status: 500 });
-//   }
-// }
+export async function PUT(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = context.params.id;
+  const updatedUser = await request.json();
+  try {
+    const usersCollection = await connectToDatabase();
+    const result = await usersCollection.updateOne(
+      { _id: new ObjectId(id) }, // Use new ObjectId(id) to convert string id to MongoDB ObjectId
+      { $set: updatedUser }
+    );
+    if (result.matchedCount === 1) {
+      return NextResponse.json({ message: "User updated successfully" });
+    } else {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+  } catch (error) {
+    console.error("Error updating user:", error);
+  }
+}
